436bdb0643300f7744308a61c7305e0e33a8d208
Douha <douhadal9@gmail.com>
2020-02-08 14:38:38 +0100

    itération 1 Douha et Michel
    Co-authored-by: Ho Michel <mho85@icloud.com>

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 class App extends Component {
+  state = {
+    contacts: contacts.splice(0, 4)
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map(contact => {
+              return (
+                <tr>
+                  <td>
+                    <img src={contact.pictureUrl} alt="contact picture" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
